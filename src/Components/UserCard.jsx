b01fbe39f3d64825ed0a8df863edd3853463bc3a
Amey <ameygawade11@gmail.com>
2025-09-23 23:39:31 +0530

    Created UserCard component to display user information.

---
+++ src/Components/UserCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const UserCard = ({user}) => {
+
+  const { firstName, lastName, photoUrl, age, gender, about} = user;
+
+  console.log(user)
+  console.log(user.photoUrl)
+  return (
+    <div>
+      <div className="card bg-gray-900 w-96 shadow-sm ">
+        <figure>
+          <img
+            src={user.photoUrl}
+            alt="Shoes"
+          />    
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{firstName + " " + lastName}</h2>
+          <p>{about}</p>
+          {age && gender ? <p>{age + " " +gender}</p> : ""}
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary">Ignore</button>
+            <button className="btn btn-secondary">Interested</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
